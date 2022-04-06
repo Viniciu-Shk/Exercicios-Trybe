dd3ae3025252951c1e57730495fdfae657e6888b
Vinicius Oliveira <vinicius.shk@gmail.com>
2022-04-06 19:03:30 -0300

    Alteraçao valores programa par

--- Fundamentos/Bloco-04-introduçao-a-javaScript-e-logica-de-programaçao/dia-01-introdução-javaScript/programa-numero-par.js
+++ Fundamentos/Bloco-04-introduçao-a-javaScript-e-logica-de-programaçao/dia-01-introdução-javaScript/programa-numero-par.js
@@ -2,7 +2,7 @@ const a = 21;
 const b = 5;
 const c = 11;
 
-if (a % 2 === 0 || b % 2 === 0 || c % 2 === 0 ) {
+if (a % 2 === 0 || b % 2 === 0 || c % 2 === 0) {
     console.log("true");
 } else {
     console.log("false");
