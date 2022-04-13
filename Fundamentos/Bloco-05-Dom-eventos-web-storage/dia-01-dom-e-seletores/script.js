cead49518add34ae7b1f99278cd470b5cc05db01
Vinicius Oliveira <vinicius.shk@gmail.com>
2022-04-13 15:46:29 -0300

    Adicionando exercícios de fixação resolvidos

---
+++ Fundamentos/Bloco-05-Dom-eventos-web-storage/dia-01-dom-e-seletores/script.js
@@ -0,0 +1,12 @@
+const paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red";
+
+document.querySelector("#page-title").innerText = 'Inceptionn';
+document.querySelector("#second-paragraph").innerText = "Um teste para tentar modicicar esse parágrafo!!";
+document.querySelector("#subtitle").innerText = "Modificação testee"
+
+let paragrafo = document.querySelectorAll(".igual");
+
+paragrafo[1].style.color = "blue";
+
+document.querySelector("#subtitle").style.color = "pink";
