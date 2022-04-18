9f3504f90fad572eb09fbd68c24c6d92388eb5ba
Vinicius Oliveira <vinicius.shk@gmail.com>
2022-04-18 19:07:56 -0300

    Resolução do exercicio 1 e 2

--- Fundamentos/Bloco-05-Dom-eventos-web-storage/dia-03-eventos/script.js
+++ Fundamentos/Bloco-05-Dom-eventos-web-storage/dia-03-eventos/script.js
@@ -10,7 +10,33 @@ function createDaysOfTheWeek() {
       weekDaysList.appendChild(dayListItem);
     };
   };
-  
+function colocaDiaMes() {
+    const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+    const mesDias = document.querySelector('#days');
+    for (let index = 0; index < dezDaysList.length; index += 1) {
+        const element = dezDaysList[index];
+        const unidadeDiaMes = document.createElement('li');
+        unidadeDiaMes.classList.add('day');
+        if (dezDaysList[index] === 25) {
+            unidadeDiaMes.classList.add('holiday', 'friday');
+        } else if (dezDaysList[index] === 24 || dezDaysList[index] === 31) {
+            unidadeDiaMes.classList.add('holiday');
+        } else if (dezDaysList[index] === 4 || dezDaysList[index] === 11 || dezDaysList[index] === 18) {
+            unidadeDiaMes.classList.add('friday');
+        }
+        unidadeDiaMes.innerHTML = element;
+        mesDias.appendChild(unidadeDiaMes);
+    }
+}
+function criaBotao(string) {
+    const divBotao = document.querySelector('.buttons-container');
+    const novoBotao = document.createElement('button');
+    novoBotao.id = 'btn-holiday';
+    novoBotao.innerText = string;
+    divBotao.appendChild(novoBotao);
+}
   createDaysOfTheWeek();
+  colocaDiaMes();
+  criaBotao('Feriados');
   
   // Escreva seu código abaixo.
