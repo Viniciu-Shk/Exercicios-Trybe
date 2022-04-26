0b30afcfe48d00e3691e03794c554844dd717b50
Vinicius Oliveira <vinicius.shk@gmail.com>
2022-04-26 18:22:19 -0300

    Conclusão dos requisitos bonus

--- Fundamentos/Bloco-06-HTML-CSS-forms-flexbox-e-responsivo/dia-1-html-css-forms/script2.js
+++ Fundamentos/Bloco-06-HTML-CSS-forms-flexbox-e-responsivo/dia-1-html-css-forms/script2.js
@@ -1,5 +1,13 @@
 let botaoEnviar = document.getElementById('btn-submit');
+let inputNome = document.getElementById('nome-user');
+let inputEmail = document.getElementById('email-user');
 botaoEnviar.addEventListener('click', prevDef);
 function prevDef(evento) {
-    evento.preventDefault();
+    let valor1 = inputNome.value;
+    let valor2 = inputEmail.value;
+    if (10 < valor1.length && valor1.length< 40 && 10 < valor2.length && valor2.length< 50) {
+        alert('Dados enviados com sucesso! Obrigado por participar do concurso TrybeTrip');
+    } else {
+        alert('Dados Inválidos');
+    }
 }
