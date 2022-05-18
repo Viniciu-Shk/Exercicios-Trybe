b1234f8fb5762d8ec03129ee2613680960f795b6
Vinicius Oliveira <vinicius.shk@gmail.com>
2022-05-18 15:18:16 -0300

    Exercícios de fixação do conteúdo do dia

---
+++ Fundamentos/Bloco-08-introdução-a-HOF/dia-4-HOF-reduce/script.js
@@ -0,0 +1,89 @@
+const numbers = [18, 19, 23, 53, 4, 5, 76, 23, 54];
+
+const somaPar = (sumEven, number) => ((number % 2 === 0) ? sumEven + number : sumEven);
+const resultSum = numbers.reduce(somaPar);
+console.log(resultSum);
+
+// parte 2
+
+const estudantes = [
+  {
+    nome: 'Jorge',
+    sobrenome: 'Silva',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 67 },
+      { name: 'Português', nota: 79 },
+      { name: 'Química', nota: 70 },
+      { name: 'Biologia', nota: 65 },
+    ],
+  },
+  {
+    nome: 'Mario',
+    sobrenome: 'Ferreira',
+    idade: 15,
+    turno: 'Tarde',
+    materias: [
+      { name: 'Matemática', nota: 59 },
+      { name: 'Português', nota: 80 },
+      { name: 'Química', nota: 78 },
+      { name: 'Biologia', nota: 92 },
+    ],
+  },
+  {
+    nome: 'Jorge',
+    sobrenome: 'Santos',
+    idade: 15,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 76 },
+      { name: 'Português', nota: 90 },
+      { name: 'Química', nota: 70 },
+      { name: 'Biologia', nota: 80 },
+    ],
+  },
+  {
+    nome: 'Maria',
+    sobrenome: 'Silveira',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 91 },
+      { name: 'Português', nota: 85 },
+      { name: 'Química', nota: 92 },
+      { name: 'Biologia', nota: 90 },
+    ],
+  },
+  {
+    nome: 'Natalia',
+    sobrenome: 'Castro',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 70 },
+      { name: 'Português', nota: 70 },
+      { name: 'Química', nota: 60 },
+      { name: 'Biologia', nota: 50 },
+    ],
+  },
+  {
+    nome: 'Wilson',
+    sobrenome: 'Martins',
+    idade: 14,
+    turno: 'Manhã',
+    materias: [
+      { name: 'Matemática', nota: 80 },
+      { name: 'Português', nota: 82 },
+      { name: 'Química', nota: 79 },
+      { name: 'Biologia', nota: 75 },
+    ],
+  },
+];
+
+const bestGrade = (higherGrade, number) => ((higherGrade.nota > number.nota ? higherGrade : number));
+const gradeList = (array) => array.map((aluno) => ({
+    aluno: aluno.nome,
+    materia: aluno.materias.reduce(bestGrade, 0).name, 
+}));
+console.log(gradeList(estudantes));
