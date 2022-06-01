8d6020169cd0fc43952412003c17d0f2ef9fbb2a
Vinicius Oliveira <vinicius.shk@gmail.com>
2022-06-01 19:16:47 -0300

    Exercício bonus resolvido

--- Front-End/bloco-9-js-e-testes-assincronos/dia-02-fetchAPI-e-async-await/api.js
+++ Front-End/bloco-9-js-e-testes-assincronos/dia-02-fetchAPI-e-async-await/api.js
@@ -1,5 +1,6 @@
 // const fetch = require('node-fetch');
 const url_api = 'https://api.coincap.io/v2/assets';
+const urlUSD = 'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd.min.json';
 const myObject = {
   method: 'GET',
   headers: { 'Accept': 'application/json' }
@@ -8,6 +9,7 @@ const olCrypto = document.getElementById("listaCrypto");
 
 const crypto = async () => {
   try {
+    const rateBRL = await convertBRL();
     const array = [];
     const response = await fetch(url_api, myObject);
     const data = await response.json();
@@ -17,13 +19,19 @@ const crypto = async () => {
     const newArray = array.filter((crypto) => crypto.rank <= 10);
     newArray.forEach((crypto) => {
       const newCoin = document.createElement('li');
-      newCoin.innerHTML = `${crypto.id} (${crypto.symbol}): ${crypto.priceUsd}`
+      newCoin.innerHTML = `${crypto.id} (${crypto.symbol}): ${crypto.priceUsd * rateBRL} reais.`
       console.log(newCoin);
       olCrypto.appendChild(newCoin);
     });
   } catch (error) {
     console.log(`Erro ocorrido: \n${error}`);
   }
+}
+
+const convertBRL = async () => {
+  const response = await fetch(urlUSD);
+  const data = await response.json();
+  return data.usd.brl;
 }
 
 window.onload = () => crypto();
