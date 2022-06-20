ab99333afd290bbca90798b13547203eb80829c4
Vinicius Oliveira <vinicius.shk@gmail.com>
2022-06-20 19:38:48 -0300

    Faltou subir 1 commit

--- Front-End/bloco-10-introducao-a-react/dia-2-componentes-react/fixation-exercises-10-2/src/App.js
+++ Front-End/bloco-10-introducao-a-react/dia-2-componentes-react/fixation-exercises-10-2/src/App.js
@@ -1,13 +1,39 @@
-import Image from './image';
+// import Image from './image';
 import './App.css';
+import React from 'react';
+import Order from './order';
 
 
-function App() {
-  return (
-    <main>
-      <Image source='https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg' alternativeText='Cute cat staring' />
-    </main>
-  );
+class App extends React.Component {
+  render() {
+    const headphone = {
+      id: 102,
+      user: "cena@gmail.com",
+      product: "Razer Headphone",
+      price: {
+        value: 99.99,
+        currency: "dollars"
+      }
+    };
+
+    const energyDrink = {
+      id: 77,
+      user: "cena@gmail.com",
+      product: "Monster 500mL",
+      price: {
+        value: 9.99,
+        currency: "dollars"
+      }
+    };
+
+    return (
+      <div className="App">
+        <h1> Orders recently created </h1>
+        <Order order={energyDrink} />
+        <Order order={headphone} />
+      </div>
+    );
+  }
 }
 
 export default App;
