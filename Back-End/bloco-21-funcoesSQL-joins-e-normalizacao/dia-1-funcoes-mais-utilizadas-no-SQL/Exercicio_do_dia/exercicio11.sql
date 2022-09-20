4cb400660e5fc85bd53b264cb9df147a84cb8086
Vinicius Oliveira <vinicius.shk@gmail.com>
2022-09-20 18:00:33 -0300

    Correção do requisito 11 para evitar alterações erroneas

--- Back-End/bloco-21-funcoesSQL-joins-e-normalizacao/dia-1-funcoes-mais-utilizadas-no-SQL/Exercicio_do_dia/exercicio11.sql
+++ Back-End/bloco-21-funcoesSQL-joins-e-normalizacao/dia-1-funcoes-mais-utilizadas-no-SQL/Exercicio_do_dia/exercicio11.sql
@@ -1,4 +1,6 @@
 -- 11. Escreva uma query que atualize a coluna PHONE_NUMBER, de modo que todos os telefones iniciados por 515 agora devem iniciar com 777.
 
 UPDATE hr.employees
-SET phone_number = REPLACE(phone_number, 515, 777);
+SET phone_number = IF(
+	LEFT(phone_number, 3) = 515, REPLACE(phone_number, LEFT(phone_number, 3), 777), phone_number
+    );
