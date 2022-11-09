7abdf6ac58fe3d46a07dc1e87b5360d039a9abd6
Vinicius Oliveira <vinicius.shk@gmail.com>
2022-11-09 16:02:51 -0300

    exercicios do dis

---
+++ Back-End/bloco-27-POO-e-SOLID/dia-1-intro-a-orientacao-a-objeto/fixacao/fixacao.ts
@@ -0,0 +1,37 @@
+class Tv {
+  private _brand: string;
+  private _size: number;
+  private _resolution: string;
+  private _connections: string;
+  private _connectedTo: string;
+
+  constructor(brand: string, size: number, resolution: string, connections: string) {
+    this._brand = brand;
+    this._size = size;
+    this._resolution = resolution;
+    this._connections = connections;
+  }
+
+  turnOn() {
+    console.log((`Criando a tv ${this._brand} ${this._size} ${this._resolution} ${this._connections}`));
+  }
+
+  get connectedTo() {
+    return this._connectedTo;
+  }
+
+  set connectedTo(newValue: string) {
+    if (this._connections.includes(newValue)) {
+      this._connectedTo = newValue;
+    } else {
+      console.log('Sorry, connection unavailable');
+      
+    }
+  }
+}
+
+const toshiba = new Tv('Toshiba', 18, 'FullHD', 'WiFi');
+
+toshiba.turnOn();
+toshiba.connectedTo = 'WiFi';
+console.log(toshiba.connectedTo);
